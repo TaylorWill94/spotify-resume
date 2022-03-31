f2f79daff0470e61ced19dc2e6f392ae30838478
Taylor W <taylorwilliams@pursuit.org>
2022-03-30 22:29:52 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="home">
+        Home
+        <br />
+        <br />
+        Search
+        <br />
+        <br />
+        Your Library
+        <br />
+        <br />
+        Create Playlist
+        <br />
+        Liked Songs
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+      </div>
     </div>
   );
 }
